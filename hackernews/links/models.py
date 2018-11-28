1fe03fac77e3b3cf8c263b3dabffc6b1970a814c
Victor Martinez <vcrmartinez@gmail.com>
2018-11-28 14:38:18 -0300

    Create link and vote models and make migration

--- hackernews/links/models.py
+++ hackernews/links/models.py
@@ -1,3 +1,11 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Link(models.Model):
+    url = models.URLField()
+    description = models.TextField(blank=True)
+
+class Vote(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    link = models.ForeignKey('links.Link', related_name='votes', on_delete=models.CASCADE)
